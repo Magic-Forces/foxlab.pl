6ec3e6b74e190c427cd4740295aeba69b35a6962
Magic-Forces <root@foxlab.pl>
2024-12-18 02:25:49 +0100

    Added responsive title font size and text shadow based on screen size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Header from "./components/header";
 import Footer from "./components/footer";
 
 export default function Home() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth <= 768);
+    };
+
+    checkScreenSize();
+
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  const styles = {
+    container: {
+      minHeight: "100vh",
+      display: "flex",
+      flexDirection: "column" as const,
+      justifyContent: "space-between",
+    },
+    main: {
+      padding: "20px",
+      textAlign: "center" as const,
+    },
+    title: {
+      fontSize: isSmallScreen ? "2.5rem" : "4.5rem",
+      color: "#FFA500",
+      textShadow: `
+      0 0 10px #FF8C00,
+      0 0 20px #FF8C00,
+      0 0 30px #FF4500,
+      0 0 40px #FF4500,
+      0 0 50px #FF6347
+    `,
+      letterSpacing: "0.2em",
+    },
+  };
+
   return (
     <div style={styles.container}>
       <Header />
@@ -14,28 +56,3 @@ export default function Home() {
     </div>
   );
 }
-
-const styles = {
-  container: {
-    minHeight: "100vh",
-    display: "flex",
-    flexDirection: "column" as const,
-    justifyContent: "space-between",
-  },
-  main: {
-    padding: "20px",
-    textAlign: "center" as const,
-  },
-  title: {
-    fontSize: "4rem",
-    color: "#FFA500",
-    textShadow: `
-      0 0 10px #FF8C00,
-      0 0 20px #FF8C00,
-      0 0 30px #FF4500,
-      0 0 40px #FF4500,
-      0 0 50px #FF6347
-    `,
-    letterSpacing: "0.2em",
-  },
-};
